6b876d257f316885ccdf41a5040865213d82c79a
Charbel <chaajaka@hotmail.com>
2026-05-26 20:54:43 +0100

    Recipe Generation setup. Created the service and some models.

---
+++ VibeCooking/Models/RecipeParameterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace VibeCooking.Models;
+
+public class RecipeParameterModel
+{
+	public List<string> Ingredients { get; set; } = new();
+	public List<string> Allergies { get; set; } = new();
+	public int DifficultyLevel { get; set; } = 1; //1 - 5?
+	
+	//TODO Other Stuff?
+}
