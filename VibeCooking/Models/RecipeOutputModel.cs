b350e55151318c04171385e39da372d2a2a9b95c
Dillon <dbarrass171103@outlook.com>
2026-05-27 20:57:23 +0100

    Added to RecipeParameterModel.cs, RecipeOutputModel.cs

--- VibeCooking/Models/RecipeOutputModel.cs
+++ VibeCooking/Models/RecipeOutputModel.cs
@@ -2,5 +2,36 @@
 
 public class RecipeOutputModel
 {
-	//TODO: Make this match the properties of the JSON structure.
+    public string RecipeName { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string CuisineType { get; set; } = string.Empty;
+    public string Difficulty { get; set; } = string.Empty;
+    public string MealType { get; set; } = string.Empty;
+    public int CookTimeMinutes { get; set; }
+    public int PrepTimeMinutes { get; set; }
+    public int Servings { get; set; }
+    public List<IngredientItem> Ingredients { get; set; } = new();
+    public List<string> Equipment { get; set; } = new();
+    public List<string> Instructions { get; set; } = new();
+    public NutritionInfo NutritionPerServing { get; set; } = new();
+    public string StorageAdvice { get; set; } = string.Empty;
+    public List<string> Substitutions { get; set; } = new();
+}
+
+public class IngredientItem
+{
+    public string Name { get; set; } = string.Empty;
+    public string Quantity { get; set; } = string.Empty;
+    public bool IsOptional { get; set; } = false;
+}
+
+public class NutritionInfo
+{
+    public int CaloriesKcal { get; set; }
+    public float ProteinG { get; set; }
+    public float CarbsG { get; set; }
+    public float FatG { get; set; }
+    public float FibreG { get; set; }
+    public float SugarG { get; set; }
+    public float SaltG { get; set; }
 }
--- VibeCooking/Models/RecipeParameterModel.cs
+++ VibeCooking/Models/RecipeParameterModel.cs
@@ -1,10 +1,73 @@
 ﻿namespace VibeCooking.Models;
 
+public enum Difficulty
+{
+	Easy,
+	Medium,
+	Hard
+}
+
+public enum MealType
+{
+	Breakfast,
+	Lunch,
+	Dinner,
+	Snack,
+	Dessert
+}
+
+public enum CuisineType
+{
+    Any,
+    American,
+    Argentinian,
+    Australian,
+    Brazilian,
+    British,
+    Caribbean,
+    Chinese,
+    Danish,
+    Emirati,
+    Filipino,
+    Finnish,
+    French,
+    German,
+    Greek,
+    HongKong,
+    Indian,
+    Indonesian,
+    Italian,
+    Japanese,
+    Korean,
+    Lebanese,
+    Malaysian,
+    Mexican,
+    Moroccan,
+    Norwegian,
+    Peruvian,
+    SaudiArabian,
+    Singaporean,
+    Spanish,
+    Swedish,
+    Taiwanese,
+    Thai,
+    Turkish,
+    Vietnamese
+}
+
 public class RecipeParameterModel
 {
 	public List<string> Ingredients { get; set; } = new();
 	public List<string> Allergies { get; set; } = new();
-	public int DifficultyLevel { get; set; } = 1; //1 - 5?
-	
-	//TODO Other Stuff?
+    public Difficulty Difficulty { get; set; } = Difficulty.Medium;
+    public MealType MealType { get; set; } = MealType.Dinner;
+    public CuisineType CuisineType { get; set; } = CuisineType.Any;
+    public int CookTimeMinutes { get; set; } = 30;
+    public int Servings {  get; set; } = 4;
+    public bool Vegetarian { get; set; } = false;
+    public bool Vegan { get; set; } = false;
+    public bool GlutenFree { get; set; } = false;
+    public bool DairyFree { get; set; } = false;
+    public string AdditionalNotes { get; set; } = string.Empty;
+
 }
