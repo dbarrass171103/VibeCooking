15bbb4da022f742490e3e598b3f1e85f7be03dfe
Charbel <chaajaka@hotmail.com>
2026-06-03 16:40:19 +0100

    Meal prep page with example view model data

---
+++ VibeCooking/ViewModels/MealPrepViewModel.cs
@@ -0,0 +1,37 @@
+﻿
+using Heron.MudCalendar;
+using Heron.MudTotalCalendar;
+using VibeCooking.Models;
+
+namespace VibeCooking.ViewModels;
+
+public class MealPrepViewModel : BaseViewModel
+{
+	public List<CalendarItem> CalendarItems { get; set; } = new();
+	public List<Value> CalendarTotalItems { get; set; } = new();
+
+	public override async Task InitAsync()
+	{
+		CalendarItems = new()
+		{
+			new CalendarItem()
+			{
+				Text = "Chicken Broth",
+				Start = DateTime.Now
+			},
+
+			new CalendarItem()
+			{
+				Text = "Tomato Soup",
+				Start = DateTime.Today.AddDays(1)
+			}
+		};
+
+		CalendarTotalItems = new()
+		{
+			new() { Date = DateTime.Today, Definition = new ValueDefinition() { Name = "chicken" }}
+		};
+	}
+
+
+}
